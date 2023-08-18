49f822ddd441c117117b2b9c4da18fa6509ce59d
Иван <platonov.vania0-0@yandex.ru>
2023-08-18 13:31:20 +0300

    IngredientController

---
+++ Controllers/IIngredientController.cs
@@ -0,0 +1,14 @@
+﻿using brandlessBar.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+namespace brandlessBar.Controllers;
+
+public interface IIngredientController
+{
+	public IActionResult Get(int id);
+	public IActionResult Post(Ingredient ingredient);
+	public IActionResult Update(Ingredient ingredient);
+	public IActionResult Delete(int id);
+
+}
---
+++ Controllers/IngredientController.cs
@@ -0,0 +1,76 @@
+﻿using brandlessBar.Data;
+using brandlessBar.Data.Models;
+using brandlessBar.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using ILogger = NLog.ILogger;
+
+namespace brandlessBar.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class IngredientController : Controller, IIngredientController
+{
+
+	private readonly IIngredientRepository _repository;
+	private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+
+	public IngredientController(IIngredientRepository repository)
+	{
+		_repository = repository;
+	}
+
+	[HttpGet]
+	[ProducesResponseType(200)]
+	[ProducesResponseType(404)]
+	public IActionResult Get(int id)
+	{
+		var ingredient = _repository.Get(id).Result;
+		if (ingredient == null)
+		{
+			return NotFound();
+		}
+		return Ok(ingredient);
+	}
+
+
+	[HttpPost]
+	[ProducesResponseType(204)]
+	[ProducesResponseType(400)]
+	public IActionResult Post(Ingredient ingredient)
+	{
+		if (_repository.Create(ingredient))
+		{
+			return StatusCode(204);
+		}
+		return BadRequest();
+	}
+
+	[HttpPut]
+	[ProducesResponseType(204)]
+	[ProducesResponseType(400)]
+	public IActionResult Update(Ingredient ingredient)
+	{
+		if (_repository.Update(ingredient))
+		{
+			return StatusCode(204);
+		}
+		return BadRequest();
+	}
+
+	[HttpDelete]
+	[ProducesResponseType(200)]
+	[ProducesResponseType(404)]
+	[ProducesResponseType(400)]
+	public IActionResult Delete(int id)
+	{
+		var ingredient = _repository.Get(id).Result;
+		if (ingredient == null)
+			return NotFound();
+		if (_repository.Delete(ingredient))
+		{
+			return Ok(ingredient);
+		}
+		return BadRequest();
+	}
+}
