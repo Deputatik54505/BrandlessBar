4eaa6476247e033d121b43814880774370947e1c
Иван <platonov.vania0-0@yandex.ru>
2023-08-23 09:50:33 +0300

    Tests for IngredientRepository are complete

--- Tests/Repositories/IngredientRepositoryTest.cs
+++ Tests/Repositories/IngredientRepositoryTest.cs
@@ -6,6 +6,10 @@ using Xunit;
 
 namespace brandlessBar.Tests.Repositories;
 
+/// <summary>
+/// The test class for Ingredient repository
+/// Expected to run all together
+/// </summary>
 public class IngredientRepositoryTest
 {
 	[Fact]
@@ -20,6 +24,55 @@ public class IngredientRepositoryTest
 		var result = repository.Create(ingredient);
 			
 		Assert.True(result);
+	}
+
+	[Fact]
+	public void Get_Apple()
+	{
+		var context = new ApplicationDbContext();
+		var repository = new IngredientRepository(context);
+		Ingredient? apple;
+		int appleId;
+
+
+		apple = context.Ingredients.FirstOrDefault(i => i.Name.Equals("apple"));
+		if (apple==null)
+			Assert.Fail();
+		appleId = apple.Id;
+		apple = repository.Get(appleId).Result;
+
+		if (apple == null)
+			Assert.Fail();
+		Assert.Equivalent(apple.Name,"apple");
+	}
+
+	[Fact]
+	public void Update_Ingredient()
+	{
+		var context = new ApplicationDbContext();
+		var repository = new IngredientRepository(context);
+		Ingredient? apple;
+
+		apple = context.Ingredients.FirstOrDefault(i => i.Name.Equals("apple"));
+		if (apple == null ) Assert.Fail();
+		apple.Description = "not as fresh apple";
+
+
+		Assert.True(repository.Update(apple));
+	}
+
+	[Fact]
+	public void Delete_Ingredient()
+	{
+		var context = new ApplicationDbContext();
+		var repository = new IngredientRepository(context);
+		Ingredient? apple;
+
+		apple = context.Ingredients.FirstOrDefault(i => i.Name.Equals("apple"));
+		if (apple==null)
+			Assert.Fail();
+
+		Assert.True(repository.Delete(apple));
 
 	}
 }
