4c7440859a6ebd57f1eef580f79b61d476e08fd2
Иван <platonov.vania0-0@yandex.ru>
2023-09-04 16:35:42 +0300

    Added general repository

--- Data/Repositories/IRepository.cs
+++ Data/Repositories/IRepository.cs
@@ -4,5 +4,11 @@ namespace brandlessBar.Data.Repositories;
 
 public interface IRepository<T> where T : SuperModel
 {
-	
+	public Task<T?> Get(int id);
+	public Task<List<T>> GetAll();
+
+	public bool Create(T entity);
+	public bool Update(T entity);
+	public bool Delete(T entity);
+
 }
---
+++ Data/Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using brandlessBar.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace brandlessBar.Data.Repositories;
+
+public class Repository<T> : IRepository<T> where T : SuperModel
+{
+	private readonly ApplicationDbContext _context;
+	private readonly DbSet<T> _entities; 
+
+	public Repository(ApplicationDbContext context)
+	{
+		_context = context;
+		_entities = context.Set<T>();
+	}
+
+	public Task<T?> Get(int id)
+	{
+		return _entities.SingleOrDefaultAsync(e => e.Id == id);
+	}
+
+	public Task<List<T>> GetAll()
+	{
+		return _entities.AsQueryable().ToListAsync();
+	}
+
+	public bool Create(T entity)
+	{
+		_entities.Add(entity);
+		return _context.SaveChanges()>0;
+	}
+
+	public bool Update(T entity)
+	{
+		_entities.Update(entity);
+		return _context.SaveChanges()>0;
+	}
+
+	public bool Delete(T entity)
+	{
+		_entities.Remove(entity);
+		return _context.SaveChanges()>0;
+	}
+}
